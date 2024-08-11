be063a4519a5b8454e99efa843f3181fb8726c28
rohan181 <46247498+rohan181@users.noreply.github.com>
2024-08-11 23:50:33 +0600

    header update

--- core/views.py
+++ core/views.py
@@ -4868,25 +4868,32 @@ def product_list_grouped_by_category(request):
 def menu_view(request):
     menus = [
         {"name": "INVOICE/BILL ENTRY", "url": "", "icon": "fa-solid fa-file-invoice"},
-        {"name": "ADMIN", "url": "/admin", "icon": "fa-solid fa-user-tie"},
+        {"name": "MRR ENTRY", "url": "/mr", "icon": "fa-solid fa-file-invoice"},
+        {"name": "EXPENSE", "url": "/expense", "icon": "fa-solid fa-coins"},
+       
+        {"name": "CLIENT LEDGER", "url": "/customerlist", "icon": "fa-solid fa-people-group"},
+        {"name": "SUPPLIER LEDGER", "url": "/suplierlist", "icon": "fa-solid fa-people-group"},
+         {"name": "INVOICE/BILL LIST", "url": "/soldlist", "icon": "fa-solid fa-file-invoice"},
         {"name": "MRR LIST", "url": "/mrinvoicelist", "icon": "fa-solid fa-file-invoice"},
-        {"name": "INVOICE/BILL LIST", "url": "/soldlist", "icon": "fa-solid fa-file-invoice"},
         {"name": "RETURN LIST", "url": "/returnlist", "icon": "fa-solid fa-list"},
+        {"name": "BILL RECEIVE LIST", "url": "bill_list", "icon": "fa-solid fa-list"},
+       
+        
         {"name": "BILL RECEIVE LIST", "url": "bill_list", "icon": "fa-solid fa-list"},
         {"name": "ADD PRODUCT", "url": "/productcreate", "icon": "fa-solid fa-plus"},
         {"name": "ADD CLIENT", "url": "/customercreate", "icon": "fa-solid fa-plus"},
         {"name": "ADD SUPPLIER", "url": "/suppliercreate", "icon": "fa-solid fa-plus"},
         {"name": "NEW EXPENSE CATEGORY", "url": "/paybillcategorycreate", "icon": "fa-solid fa-plus"},
         {"name": "NEW CORPORATE CATEGORY", "url": "/corpocategorycreate", "icon": "fa-solid fa-plus"},
-        {"name": "CLIENT LEDGER", "url": "/customerlist", "icon": "fa-solid fa-people-group"},
-        {"name": "SUPPLIER LEDGER", "url": "/suplierlist", "icon": "fa-solid fa-people-group"},
-        {"name": "MRR ENTRY", "url": "/mr", "icon": "fa-solid fa-file-invoice"},
-        {"name": "EXPENSE", "url": "/expense", "icon": "fa-solid fa-coins"},
+        
+        
+        
         {"name": "DAILY REPORT", "url": "/daily", "icon": "fa-solid fa-file-pen"},
         {"name": "SALES REPORT", "url": "/salesreport", "icon": "fa-solid fa-file-pen"},
         {"name": "EXPENSE REPORT", "url": "/expensereport", "icon": "fa-solid fa-wallet"},
         {"name": "PRODUCT REPORT", "url": "/plreport", "icon": "ion-icon name='documents-outline'"},
         {"name": "CURRENT PRODUCT", "url": "/currentproduct", "icon": "ion-icon name='documents-outline'"},
+        {"name": "ADMIN", "url": "/admin", "icon": "fa-solid fa-user-tie"},
         {"name": "SMS", "url": "/sms", "icon": "fa-solid fa-comments"},
     ]
     return render(request, 'core/menu.html', {'menus': menus})    
